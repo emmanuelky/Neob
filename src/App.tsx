91a1954e2bdfbed650157cdc3056f59f41845747
Emmanuel Iyere <40471024+emmanuelky@users.noreply.github.com>
2021-11-16 04:05:12 +0100

    sorted asteroid spreadsheet by latest

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import axios from 'axios'
-import { Neows } from './types';
+import { Neows, NearEarthObject } from './types';
+import { Container, Col, Row } from 'react-bootstrap'
 
 
 
@@ -21,9 +22,75 @@ function App() {
 
 
   return (
-    <div className="">
+    <Container fluid>
+      <Row>
+        <Col md={12}>
 
-    </div>
+          {
+            isLoading && <div><h4>Loading...</h4></div>
+          }
+          {
+            isError && <div><h4>An error occured...</h4></div>
+
+          }
+
+        </Col>
+      </Row>
+      <Row>
+        <Col md={2}>
+          <h3>Time/Date</h3>
+        </Col>
+        <Col md={2}>
+          <h3>Asteroid name</h3>
+        </Col>
+        <Col md={2}>
+          <h3>Potential Hazard</h3>
+        </Col>
+        <Col md={2}>
+          <h3>Estimated Diameter (km)</h3>
+        </Col>
+        <Col md={2}>
+          <h3>Miss distance (km)</h3>
+        </Col>
+        <Col md={2}>
+          <h3>Velocity (km/h)</h3>
+        </Col>
+
+      </Row>
+
+
+      {
+
+        (Object as any).values(data?.near_earth_objects).flat()
+          .slice(0, 10)
+          .sort((a: any, b: any) => {
+            const closestApproachDate = a.close_approach_data[0].close_approach_date_full
+            const lastApproachDate = b.close_approach_data[0].close_approach_date_full
+
+            return closestApproachDate.localeCompare(lastApproachDate);
+
+          })
+          .map((item: NearEarthObject) => (
+            <Row key={item.id}>
+
+              <Col md={2}>{
+                item.close_approach_data[0].close_approach_date_full
+              }</Col>
+              <Col md={2}>{item.name}</Col>
+              <Col md={2}>{
+                item.is_potentially_hazardous_asteroid ? 'Yes' : 'No'
+              }</Col>
+              <Col md={2}>{item.estimated_diameter.meters.estimated_diameter_max}</Col>
+              <Col md={2}>{item.close_approach_data[0].miss_distance.kilometers}</Col>
+              <Col md={2}>{item.close_approach_data[0].relative_velocity.kilometers_per_hour}</Col>
+
+            </Row>
+          )
+          )
+      }
+
+
+    </Container>
   );
 }
 
